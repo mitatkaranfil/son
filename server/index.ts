435bf49661ce4c2b5f4e448ac5d12180be3de557
mitatkaranfil <your-email@example.com>
2025-03-31 14:21:41 +0300

    PORT ortam değişkeni eklendi Railway uyumluluğu için

--- server/index.ts
+++ server/index.ts
@@ -56,10 +56,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 3000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 3000;
+  // Use environment variable PORT or default to 3000
+  const port = process.env.PORT || 3000;
   server.listen({
     port,
     host: "0.0.0.0",
